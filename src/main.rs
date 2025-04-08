2226458a7cdd68846f86239a5cd57a06c7fe729a
aethne0 <nah>
2025-04-08 18:02:26 -0400

    clean up

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
 #![feature(test)]
 #![macro_use]
 
-use std::fs;
-
-use io::ReaderWriter;
-
-#[allow(dead_code, unused)]
-mod io;
-
 #[macro_export]
 macro_rules! s {
     ($s:expr) => {
@@ -16,13 +9,5 @@ macro_rules! s {
 }
 
 fn main() {
-    fs::remove_file("test.bl").unwrap();
-
-    let mut rw = ReaderWriter::new().unwrap();
-    let _ = rw.put(b"asd1", b"zxc1");
-    let _ = rw.put(b"asd2", b"zxc2");
-    let _ = rw.put(b"asd3", b"zxc2");
-
-    let x = rw.get_last(b"asd2").unwrap();
-    dbg!(x);
+    println!("yo yo");
 }
